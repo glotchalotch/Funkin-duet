eec7aef8b49c70e6462e4e80c124601c19c35272
glotch <30377049+glotchalotch@users.noreply.github.com>
2021-03-26 16:40:48 -0700

    added runtime window title changing

--- source/Main.hx
+++ source/Main.hx
@@ -1,14 +1,22 @@
 package;
 
+import openfl.Assets;
 import flixel.FlxGame;
 import openfl.display.FPS;
 import openfl.display.Sprite;
+import lime.app.Application;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
+
+		#if desktop
+		var text:String = Assets.getText("assets/data/windowTitle.txt");
+		if(text != null) Application.current.window.title = text;
+		#end
+
 		addChild(new FlxGame(0, 0, TitleState, 1, 60, 120, true));
 
 		#if !mobile
